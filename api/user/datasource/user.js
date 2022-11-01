9e22ab59a64e7d42d92d07e59cec92e240a0c7cf
pedropeglow <pedropeglowm@gmail.com>
2022-11-01 17:29:32 -0300

    :sparkles: Adicionando Custom Scalars

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -7,11 +7,48 @@ class UserAPI extends RESTDataSource {
   }
 
   async getUsers() {
-    return this.get("/users");
+    const users = await this.get("/users");
+    return users.map(async (user) => ({
+      id: user.id,
+      nome: user.nome,
+      email: user.email,
+      ativo: user.ativo,
+      role: await this.get(`/roles/${user.role}`),
+    }));
   }
 
   async getUserById(id) {
-    return this.get(`/users/${id}`);
+    const user = await this.get(`/users/${id}`);
+    user.role = await this.get(`/roles/${user.role}`);
+    return user;
+  }
+
+  async adicionaUser(user) {
+    const users = await this.get("/users");
+    user.id = users.length + 1;
+    const role = await this.get(`roles?type=${user.role}`);
+    await this.post("users", { ...user, role: role[0].id });
+    return {
+      ...user,
+      role: role[0],
+    };
+  }
+
+  async atualizaUser(novosDados) {
+    const role = await this.get(`roles?type=${novosDados.role}`);
+    await this.put(`users/${novosDados.id}`, {
+      ...novosDados,
+      role: role[0].id,
+    });
+    return {
+      ...novosDados,
+      role: role[0],
+    };
+  }
+
+  async deletaUser(id) {
+    await this.delete(`users/${id}`);
+    return id;
   }
 }
 
--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,8 +1,25 @@
+const { GraphQLScalarType } = require("graphql");
+
 const userResolvers = {
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "string de data e hora no fromato ISO-8601",
+    serialize: (value) => value.toISOString(),
+    parseValue: (value) => new Date(value),
+    parseLiteral: (ast) => new Date(ast.value),
+  }),
   Query: {
     users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
     user: (root, { id }, { dataSources }) =>
       dataSources.usersAPI.getUserById(id),
+  },
+  Mutation: {
+    adicionaUser: async (root, user, { dataSources }) =>
+      dataSources.usersAPI.adicionaUser(user),
+    atualizaUser: async (root, novosDados, { dataSources }) =>
+      dataSources.usersAPI.atualizaUser(novosDados),
+    deletaUser: (root, { id }, { dataSources }) =>
+      dataSources.usersAPI.deletaUser(id),
   },
 };
 
